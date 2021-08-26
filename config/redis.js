d3bc056ca1a8104d33b3d393987e96d8e2cedfe6
budukhyash <budukhyash@gmail.com>
2021-08-26 23:10:51 +0530

    adding password

--- config/redis.js
+++ config/redis.js
@@ -3,6 +3,7 @@ import redis from "redis";
 export const client = redis.createClient({
 	host: process.env.REDIS_HOST,
 	port: process.env.REDIS_PORT,
+	password: process.env.REDIS_PASSWORD
 });
 
 client.on("error", (err) => {
